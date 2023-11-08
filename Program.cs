7ce8b859f7671146e98ee93a1111bb3bb99cc806
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-08 19:20:08 +0100

    Added services for updating user-data.json

--- Program.cs
+++ Program.cs
@@ -6,21 +6,10 @@ namespace ConsoleNewsletterGenerator
 {
     class Program
     {
-        static List<User> FetchUsersFromJson()
-        {
-            var currentDirectory = Directory.GetCurrentDirectory();
-            string jsonPath = Path.Combine(currentDirectory, @"..\..\..\Data\user-data.json");
-            string jsonData = File.ReadAllText(jsonPath);
-            return JsonConvert.DeserializeObject<List<User>>(jsonData);
-        }
+        
         static void Main(string[] args)
         {
-            List<User> users = FetchUsersFromJson();
             
-            foreach (var user in users)
-            {
-                Console.WriteLine(user.Name);
-            }
         }
     }
 }
