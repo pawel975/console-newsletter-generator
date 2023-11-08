b10be1bd846484b6c488134bcfba08563ba095a6
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-08 18:15:26 +0100

    Added user model

---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_newsletter_generator
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Discount { get; set; }
+        public DateTime MemeberSinceDate { get; set; }
+    }
+}
