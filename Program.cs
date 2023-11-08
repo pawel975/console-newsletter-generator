0fa222c487c44dfd2298cdaa5c70fa1ce719c89c
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-08 19:04:24 +0100

    Deserialization mechanism for users data json

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
 ﻿
+using console_newsletter_generator;
+using Newtonsoft.Json;
+
 namespace ConsoleNewsletterGenerator
 {
-    class program
+    class Program
     {
+        static List<User> FetchUsersFromJson()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            string jsonPath = Path.Combine(currentDirectory, @"..\..\..\Data\user-data.json");
+            string jsonData = File.ReadAllText(jsonPath);
+            return JsonConvert.DeserializeObject<List<User>>(jsonData);
+        }
         static void Main(string[] args)
         {
-
+            List<User> users = FetchUsersFromJson();
+            
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Name);
+            }
         }
     }
 }
