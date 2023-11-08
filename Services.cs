b02cd3391bcd8d5087899e2649ab9399085b6573
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-08 19:21:53 +0100

    Namespace name update

--- Services.cs
+++ Services.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace console_newsletter_generator
+namespace ConsoleNewsletterGenerator
 {
     public class Services
     {
--- User.cs
+++ User.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace console_newsletter_generator
+namespace ConsoleNewsletterGenerator
 {
     public class User
     {
