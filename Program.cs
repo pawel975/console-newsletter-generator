7da1a8baaeafb320a574943e44f8e008d85afbe1
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-08 18:10:44 +0100

    basic program.cs structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace ConsoleNewsletterGenerator
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
