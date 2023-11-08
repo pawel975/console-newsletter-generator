65c487cb254ae6dde853a2255d0eb2d95f137f4b
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-08 22:09:04 +0100

    NewsletterGenerator class read html template

--- NewsletterGenerator.cs
+++ NewsletterGenerator.cs
@@ -8,6 +8,28 @@ namespace ConsoleNewsletterGenerator
 {
     public class NewsletterGenerator
     {
+        // TODO: Possibly will delete this property
+        static private string HTMLTemplate { get; set; }
+
+        // TODO: Possibly will change this function to return html file string
+        static void ReadHTMLTemplate(string templatePath)
+        {
+            //TODO: Check if template has .html extension
+            string fileExtension = Path.GetExtension(templatePath);
+
+            if (File.Exists(templatePath) && fileExtension.Equals(".html", StringComparison.OrdinalIgnoreCase))
+            {
+                File.WriteAllText(templatePath, HTMLTemplate);
+            }
+        }
+
+        // TODO: placeholders dictionary should contain placeholder-value pairs
+        static string FillTemplate(Dictionary<string, string> placeholders) 
+        {
+            string filledTemplate = default;
+
+            return filledTemplate;
+        }
     }
 
 }
