9e56d92b177ee7dc216dfea3138b15c958d045cd
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-09 23:06:29 +0100

    menu message added

--- Program.cs
+++ Program.cs
@@ -3,9 +3,10 @@ namespace ConsoleNewsletterGenerator;
 
 class Program
 {
+    private const string MenuMessage = """ ***** Newsletter Generator *****""";
 
     static void Main(string[] args)
     {
-
+        Console.WriteLine(MenuMessage);
     }
 }
