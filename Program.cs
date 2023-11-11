65479665b1877322a57ad3a3fc1c2812e46e298f
Paweł Kurek <pawel.kurek975@gmail.com>
2023-11-11 09:31:17 +0100

    Added mechanism for html saving

--- Program.cs
+++ Program.cs
@@ -4,18 +4,19 @@ namespace ConsoleNewsletterGenerator;
 class Program
 {
     private const string MenuMessage = """ ***** Newsletter Generator *****""";
+    static private readonly string templatePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Templates\newsletter-template.html"));
 
     static void Main(string[] args)
     {
         Console.WriteLine(MenuMessage);
 
+        // TODO: Ask for user id / name an then fetch user
         User user = Services.GetUserById(1);
 
         NewsletterGenerator newsletterGenerator = new NewsletterGenerator();
-        
-        string templatePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Templates\newsletter-template.html"));
-
         var newsletter = newsletterGenerator.GenerateNewsletter(templatePath, user);
-        File.WriteAllText(@"C:\Users\pawel\Desktop\example.html", newsletter);
+
+        // TODO: Ask for path to save file
+        newsletter.SaveDataToHTMLFile(@"C:\Users\pawel\Desktop\example.html");
     }
 }
---
+++ Utils.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleNewsletterGenerator
+{
+    static public class Utils
+    {
+        /// <summary>
+        /// Saves string data into an HTML file for a given path.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="filePath"></param>
+        public static void SaveDataToHTMLFile(this string data, string filePath)
+        {
+            try
+            {
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(data);
+
+                htmlDocument.Save(filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
